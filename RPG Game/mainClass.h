0b0ff3c68d9d32fe97c4d19a6cce75f87e55a319
DinoRUS <104258772+DinoRUS@users.noreply.github.com>
2025-11-06 22:24:57 +0400

    Update mainClass.h

--- RPG Game/mainClass.h
+++ RPG Game/mainClass.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 
 class Npc
@@ -14,89 +15,26 @@ protected: //модификатор 0 защищенный (дает досту�
     unsigned short lvl{ 1 };
 
 public:    //публичный модификатор доступ (использовать метод можно в любом месте)
-    string GetName()
-    {
-        return name;
-    }
-    unsigned int GetHealth()
-    {
-        return health;
-    }
-    float GetDamage()
-    {
-        return damage;
-    }
-    unsigned int GetLvl()
-    {
-        return lvl;
-    }
-    virtual void GetInfo() //метод класса
-    {
-        cout << "Имя - " << name << endl;
-        cout << "Здоровье - " << health << endl;
-        cout << "Урон - " << damage << endl;
-    }
+    string GetName();
+    unsigned int GetHealth();
+    float GetDamage();
+    unsigned int GetLvl();
+    virtual void GetInfo();
 
     virtual void Create() {};
-    virtual bool Save()
-    {
-
-        ofstream saveSystem("save.bin", ios::binary);
-        if (saveSystem.is_open())
-        {
-            saveSystem.write(reinterpret_cast<const char*>(&name), sizeof(name));
-            saveSystem.write(reinterpret_cast<const char*>(&health), sizeof(health));
-            saveSystem.write(reinterpret_cast<const char*>(&damage), sizeof(damage));
-            saveSystem.write(reinterpret_cast<const char*>(&lvl), sizeof(lvl));
-            saveSystem.close();
-            return true;
-        }
-        else
-        {
-            cout << "сохранение не удалось" << endl;
-            return false;
-        }
-        saveSystem.close();
-    };
-    Npc: Load()
-    {
-        ifstream loadSystem("save.bin", ios::binary);
-        Npc npc; //временное хранилище для считывания данных из файла
-        if (loadSystem.is_open())
-        {
-            loadSystem.read(reinterpret_cast<char*>(&npc.name), sizeof(npc.name));
-            loadSystem.read(reinterpret_cast<char*>(&npc.health), sizeof(npc.health));
-            loadSystem.read(reinterpret_cast<char*>(&npc.damage), sizeof(npc.damage));
-            loadSystem.read(reinterpret_cast<char*>(&npc.lvl), sizeof(npc.lvl));
-        }
-        else
-        {
-            cout << "связь с базой нарушена\nПамять утерена" << endl;
-            return npc;
-        }
-        loadSystem.close();
-        return npc;
-
-
-    };
+    virtual bool Save();
+   
+    Npc Load();
+    
 
 
 
 };
-
 class Player
 {
 public:
-    void Create(Npc* player)
-    {
-        player->Create();
-    }
-    void Save(Npc* player)
-    {
-        player->Save();
-    }
-    void Load(Npc* player)
-    {
-        player->Load();
-    }
+    void Create(Npc* player);
+    void Save(Npc* player);
+    void Load(Npc* player);
+    
 };
