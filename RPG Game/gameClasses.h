3cbb0752db09cba98884f379d146427c55d6a7c0
DinoRUS <104258772+DinoRUS@users.noreply.github.com>
2025-11-06 22:28:27 +0400

    Update gameClasses.h

--- RPG Game/gameClasses.h
+++ RPG Game/gameClasses.h
@@ -1,256 +1,107 @@
-#pragma once
 #include "mainClass.h"
-
-//наследование - использование доступных свойств
-// и методов класса родителям (parent), классом наследником(child)
-class Warrior : public virtual Npc //наследование с модификатором доступа public
+bool Warrior::Save() 
 {
-protected:  //модификатор 0 (приватный - защищенный, доступ к полям, только внутри класса)
-    unsigned short strenght{ 31 };
-    string weapons[4] = { "кастет", "дубинка", "клинок", "меч" };
-public:
-    //конструктор - метод, который вызывается в момент создания экземпляра
-    //класса (вручную вызвать в основном потоке программы не можем)
-    bool Save() override
+
+
+    if (Npc::Save())
     {
-
-
-        if (Npc::Save())
+        ofstream saveSystem("save.bin", ios::binary);
+        if (saveSystem.is_open())
         {
-            ofstream saveSystem("save.bin", ios::binary);
-            if (saveSystem.is_open())
-            {
 
-                saveSystem.write(reinterpret_cast<const char*>(&strenght), sizeof(strenght));
-                for (int i = 0; i < 4; i++)
-                {
-                    saveSystem.write(reinterpret_cast<const char*>(&weapons[i]), sizeof(weapons[i]));
-                }
-                saveSystem.close();
-                return true;
-            }
-            else
-            {
-                cout << "сохранение не удалось" << endl;
-                return false;
-            }
-        }
-    };
-    Warrior Load()
-    {
-        ifstream loadSystem("save.bin", ios::binary);
-        Warrior warrior; //временное хранилище для считывания данных из файла
-        warrior = Npc::Load();
-        if (loadSystem.is_open())
-        {
-            loadSystem.read(reinterpret_cast<char*>(&strenght), sizeof(strenght));
+            saveSystem.write(reinterpret_cast<const char*>(&strenght), sizeof(strenght));
             for (int i = 0; i < 4; i++)
             {
-                loadSystem.read(reinterpret_cast<char*>(&weapons[i]), sizeof(weapons[i]));
+                saveSystem.write(reinterpret_cast<const char*>(&weapons[i]), sizeof(weapons[i]));
             }
+            saveSystem.close();
+            return true;
         }
         else
         {
-            cout << "связь с базой нарушена\nПамять утерена" << endl;
-            return warrior;
+            cout << "сохранение не удалось" << endl;
+            return false;
         }
-        loadSystem.close();
+    }
+};
+Warrior Warrior::Load()
+{
+    ifstream loadSystem("save.bin", ios::binary);
+    Warrior warrior; //временное хранилище для считывания данных из файла
+    warrior = Npc::Load();
+    if (loadSystem.is_open())
+    {
+        loadSystem.read(reinterpret_cast<char*>(&strenght), sizeof(strenght));
+        for (int i = 0; i < 4; i++)
+        {
+            loadSystem.read(reinterpret_cast<char*>(&weapons[i]), sizeof(weapons[i]));
+        }
+    }
+    else
+    {
+        cout << "связь с базой нарушена\nПамять утерена" << endl;
         return warrior;
+    }
+    loadSystem.close();
+    return warrior;
 
 
-    };
-
-
-    Warrior() //конструктор по умолчанию, когда нет аргументов
-    {
-        name = "воин";
-        health = 35;
-        damage = 10;
-    }
-    //кастомный конструктор
-    Warrior(string name, unsigned int health, float damage)
-    {
-        cout << "кастомный конструктор война" << endl;
-        this->name = name;
-        this->health = health;
-        this->damage = damage;
-    }
-
-    void GetWeapons()
-    {
-        cout << name << " взял в руки " << weapons[lvl - 1];
-    }
-    void GetInfo() override  //полиморфизм (перегрузка для метода)
-    {
-        Npc::GetInfo();
-        cout << "Сила - " << strenght << endl;
-        cout << "Доступное оружие - ";
-        for (int i = 0; i < lvl; i++)
-        {
-            cout << weapons[i] << endl;
-        }
-    }
-    void Create() override
-    {
-        cout << "Вы создали война" << endl;
-        cout << "Введите имя персонажа\t";
-        cin >> name;
-        GetInfo();
-        GetWeapons();
-    }
-    //перегрузка операторов
-    //перегрузка оператора сравнения (==)
-
-    bool operator == (const Warrior& warrior) const
-    {
-        return ((warrior.damage == this->damage) && (warrior.health == this->health)
-            && (warrior.strenght == this->strenght));
-    }
-    void operator = (Npc npc)
-    {
-        this->name = npc.GetName();
-        this->name = npc.GetHealth();
-        this->name = npc.GetDamage();
-        this->name = npc.GetLvl();
-    }
-
-    //деструктор - метод, который вызывается автоматически при высвобождении памяти
-    //при окончании работы с экземпляром класса (нельзя вызвать вручную)
-    ~Warrior() //деструктор всегда без аргументов
-    {
-        cout << name << " пал смертью храбрых" << endl;
-    }
 };
 
-//virtual - создает виртуализацию методов, классов
-//при этом сам класс повторно не создается
-class Wizard : public virtual Npc
+
+Warrior::Warrior() //конструктор по умолчанию, когда нет аргументов
 {
-protected:
-    unsigned short intellect = 27;
-    string spell[4] = { "вспышка", "магисческая стрела", "огненный шар", "метеоритный дождь" };
-public:
-    Wizard()
-    {
-        name = "волшебник";
-        health = 23;
-        damage = 15;
-    }
-    Wizard(string name, unsigned int health, float damage)
-    {
-        cout << "кастомный конструктор волшебника" << endl;
-        this->name = name;
-        this->health = health;
-        this->damage = damage;
-    }
-    void GetInfo() override  //полиморфизм (перегрузка для метода)
-    {
-        Npc::GetInfo();
-        cout << "Интеллект - " << intellect << endl;
-        cout << "Доступные заклинания в книге заклинаний - ";
-        for (int i = 0; i < lvl; i++)
-        {
-            cout << spell[i] << endl;
-        }
-    }
-    void CastSpell()
-    {
-        cout << name << " применяет " << spell[lvl - 1] << endl;
-    }
-    void Create() override
-    {
-        cout << "Вы создали волшебника" << endl;
-        cout << "Введите имя персонажа\t";
-        cin >> name;
-        GetInfo();
-        CastSpell();
-    }
-    bool Save() override
-    {
-
-
-        if (Npc::Save())
-        {
-            ofstream saveSystem("save.bin", ios::binary);
-            if (saveSystem.is_open())
-            {
-
-                saveSystem.write(reinterpret_cast<const char*>(&intellect), sizeof(intellect));
-                for (int i = 0; i < 4; i++)
-                {
-                    saveSystem.write(reinterpret_cast<const char*>(&spell[i]), sizeof(spell[i]));
-                }
-                saveSystem.close();
-                return true;
-            }
-            else
-            {
-                cout << "сохранение не удалось" << endl;
-                return false;
-            }
-        }
-    }
-    ~Wizard() //деструктор всегда без аргументов
-    {
-        cout << name << " испустил дух" << endl;
-    }
-
-};
-
-//множественное наследование
-class Paladin : public Warrior, public Wizard
-    //следующий родительственный класс добавляется через запятую
+    name = "воин";
+    health = 35;
+    damage = 10;
+}
+//кастомный конструктор
+Warrior::Warrior(string name, unsigned int health, float damage)
 {
-public:
-    Paladin()
-    {
-        name = "паладин";
-        health = 25;
-        damage = 12;
-        strenght = 27;
-    }
-    void GetInfo() override
-    {
-        Warrior::GetInfo();
-        cout << "Интеллект - " << intellect << endl;
-        cout << "Доступные заклинания в книге заклинаний - ";
-        for (int i = 0; i < lvl; i++)
-        {
-            cout << spell[i] << endl;
-        }
-    }
-    void Create() override
-    {
-        cout << "Вы создали паладина" << endl;
-        cout << "Введите имя персонажа\t";
-        cin >> name;
-        GetInfo();
-        CastSpell();
-        GetWeapons();
-    }
-    bool Save() override
-    {
+    cout << "кастомный конструктор война" << endl;
+    this->name = name;
+    this->health = health;
+    this->damage = damage;
+}
 
-
-        if (Npc::Save())
-        {
-            ofstream saveSystem("save.bin", ios::binary);
-            if (saveSystem.is_open())
-            {
-
-                saveSystem.write(reinterpret_cast<const char*>(&intellect), sizeof(intellect));
-                for (int i = 0; i < 4; i++)
-                {
-                    saveSystem.write(reinterpret_cast<const char*>(&spell[i]), sizeof(spell[i]));
-                }
-                saveSystem.close();
-                return true;
-            }
-            else
-            {
-                cout << "сохранение не удалось" << endl;
-                return false;
-            }
-        }
+void Warrior::GetWeapons()
+{
+    cout << name << " взял в руки " << weapons[lvl - 1];
+}
+void Warrior::GetInfo()   //полиморфизм (перегрузка для метода)
+{
+    Npc::GetInfo();
+    cout << "Сила - " << strenght << endl;
+    cout << "Доступное оружие - ";
+    for (int i = 0; i < lvl; i++)
+    {
+        cout << weapons[i] << endl;
     }
-};
+}
+void Warrior::Create() 
+{
+    cout << "Вы создали война" << endl;
+    cout << "Введите имя персонажа\t";
+    cin >> name;
+    GetInfo();
+    GetWeapons();
+}
+bool Warrior::operator == (const Warrior& warrior) const
+{
+    return ((warrior.damage == this->damage) && (warrior.health == this->health)
+        && (warrior.strenght == this->strenght));
+}
+void Warrior::operator = (Npc npc)
+{
+    this->name = npc.GetName();
+    this->name = npc.GetHealth();
+    this->name = npc.GetDamage();
+    this->name = npc.GetLvl();
+}
+
+//деструктор - метод, который вызывается автоматически при высвобождении памяти
+//при окончании работы с экземпляром класса (нельзя вызвать вручную)
+Warrior::~Warrior() //деструктор всегда без аргументов
+{
+    cout << name << " пал смертью храбрых" << endl;
+}
