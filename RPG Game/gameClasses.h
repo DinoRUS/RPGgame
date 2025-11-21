aafc6d66a719b3a76bef12ec08ac33705e03644b
DinoRUS <104258772+DinoRUS@users.noreply.github.com>
2025-11-21 09:58:36 +0400

    Update gameClasses.h

--- RPG Game/gameClasses.h
+++ RPG Game/gameClasses.h
@@ -1,5 +1,6 @@
 #pragma once
-#include "gameClasses.h"
+#include "mainClass.cpp"
+#include <string>
 
 //наследование - использование доступных свойств
 // и методов класса родителям (parent), классом наследником(child)
@@ -15,15 +16,15 @@ public:
     //кастомный конструктор
     Warrior(string name, unsigned int health, float damage);
 
-    void GetWeapons() 
+    void GetWeapons()
     {
 
     };
-    void GetInfo() override 
+    void GetInfo() override
     {
 
     };  //полиморфизм (перегрузка для метода)
-    void Create() override 
+    void Create() override
     {
 
     };
@@ -31,10 +32,10 @@ public:
     //перегрузка операторов
     //перегрузка оператора сравнения (==)
 
-    bool operator == (const Warrior& warrior) const{
+    bool operator == (const Warrior& warrior) const {
         return (wairror.damage == this->damage) &&
-               (wairror.health == this->health) &&
-               (wairror.strength == this->strength);
+            (wairror.health == this->health) &&
+            (wairror.strength == this->strength);
     }
 
     Warrior& operator = (const Npc& npc)
@@ -51,7 +52,7 @@ public:
         }
     }
     void operator = (Npc npc);
-    bool Save() override 
+    bool Save() override
     {
         ofstream saveSystem("save.bin", ios::binary);
         if (saveSystem.is_open())
@@ -93,7 +94,7 @@ public:
         return true;
     };
 
-    ~Warrior() 
+    ~Warrior()
     {
 
     }; //деструктор всегда без аргументов
@@ -105,89 +106,76 @@ public:
 //при этом сам класс повторно не создается
 class Wizard : public virtual Npc
 {
+    class Spell
+    {
+    protected:
+        string name{ "заклинание" };
+        unsigned short damage{ 0 };
+        unsigned short price{ 0 };
+        bool isCurse{ false };
+        int timeCast{ 0 };
+    public:
+
+        Spell(string name = "заклинание", unsigned short damage = 0,
+            unsigned short price = 0, bool isCurse = false, int timeCast = 0)
+        {
+            this->name = name;
+            this->damage = damage;
+            this->price = price;
+            this->isCurse = isCurse;
+            this->timeCast = timeCast;
+        }
+
+        string operator[](unsigned index) const
+        {
+            switch (index)
+            {
+            case 0: return "Название - " + name; break;
+            case 1: return "Урон - " + to_string(damage); break;
+            case 2: return  "Стоймость применения -" + to_string(price) + "маны"; break;
+            case 3:
+            {
+                if (isCurse)
+                {
+                    return "периодичный урон";
+                }
+                else {
+                    return "произносимое заклинание";
+                }
+                break;
+            }
+            case 4: return isCurse ? "Длительность негативного эффекта - " + to_string(timeCast) :
+                "Длительность применения - " + to_string(timeCast);
+                break;
+            default: return "такого свойства заклинания не существует";
+                break;
+            }
+        }
+
+        unsigned short CastSpell()
+        {
+            cout << "Вы применили " << name << " на противнике" << endl;
+            return damage;
+        }
+    };
 protected:
     unsigned short intellect = 27;
-    string spell[4] = { "вспышка", "магисческая стрела", "огненный шар", "метеоритный дождь" };
+    Spell spells[5] = {
+        Spell("Лазер",20,50,false,3)
+        Spell("Заморзка",10,30,false,1)
+        Spell("Вулкан",10,20,false)
+        Spell("Метеор",10,25,true,5)
+        Spell("Вихрь",5,10,false,10)
+    };
 public:
-    Wizard()
-    {
-        name = "волшебник";
-        health = 23;
-        damage = 15;
-    }
-    Wizard(string name, unsigned int health, float damage)
-    {
-        cout << "кастомный конструктор волшебника" << endl;
-        this->name = name;
-        this->health = health;
-        this->damage = damage;
-    }
-    void GetInfo() override  //полиморфизм (перегрузка для метода)
-    {
-        Npc::GetInfo();
-        cout << "Интеллект - " << intellect << endl;
-        cout << "Доступные заклинания в книге заклинаний - ";
-        for (int i = 0; i < lvl; i++)
-        {
-            cout << spell[i] << endl;
-        }
-    }
-    void CastSpell()
-    {
-        cout << name << " применяет " << spell[lvl - 1] << endl;
-    }
-    void Create() override
-    {
-        cout << "Вы создали волшебника" << endl;
-        cout << "Введите имя персонажа\t";
-        cin >> name;
-        GetInfo();
-        CastSpell();
-    }
-    bool operator == (const Wizard& wizard) const
-    {
-        return ((wizard.damage == this->damage) && (wizard.health == this->health)
-            && (wizard.intellect == this->intellect));
-    }
-    void operator = (Npc npc)
-    {
-        this->name = npc.GetName();
-        this->name = npc.GetHealth();
-        this->name = npc.GetDamage();
-        this->name = npc.GetLvl();
-    }
-    bool Save() override
-    {
-
-
-        if (Npc::Save())
-        {
-            ofstream saveSystem("save.bin", ios::binary);
-            if (saveSystem.is_open())
-            {
-
-                saveSystem.write(reinterpret_cast<const char*>(&intellect), sizeof(intellect));
-                for (int i = 0; i < 4; i++)
-                {
-                    saveSystem.write(reinterpret_cast<const char*>(&spell[i]), sizeof(spell[i]));
-                }
-                saveSystem.close();
-                return true;
-            }
-            else
-            {
-                cout << "сохранение не удалось" << endl;
-                return false;
-            }
-        }
-    }
-    ~Wizard() //деструктор всегда без аргументов
-    {
-        cout << name << " испустил дух" << endl;
-    }
-
-};
-
+    Wizard();
+    Wizard(string name, unsigned int health, float damage);
+    void GetInfo() override;  //полиморфизм (перегрузка для метода)
+    void Create() override;
+    Wizard operator + (const Wizard& wizard) const;
+    bool Save() override;
+    ~Wizard();
+}
 //множественное наследование
 class Paladin : public Warrior, public Wizard
     //следующий родительственный класс добавляется через запятую
