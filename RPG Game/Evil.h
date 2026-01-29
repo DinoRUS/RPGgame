60432e79f4cb62cac09f367a691c01950f9f0430
DinoRUS <104258772+DinoRUS@users.noreply.github.com>
2026-01-30 02:21:56 +0400

    Update Evil.h

--- RPG Game/Evil.h
+++ RPG Game/Evil.h
@@ -1,1 +1,11 @@
+#pragma once
+#include "MainClass.h"
+#include <iostream>
 
+class ThePriestess : public Npc {
+public:
+    ThePriestess(const std::string& name);
+    ThePriestess(const std::string& name, unsigned int health);
+    ThePriestess(const std::string& name, unsigned int health, float damage);
+    void GetInfo();
+};
